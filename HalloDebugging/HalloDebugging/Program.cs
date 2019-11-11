5dd7faa206ac0fda1e2bbabf6b6a88a3b1eb1a9b
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-11 11:19:47 +0100

    Haltepunkte und Einzelschrittmodus

--- HalloDebugging/HalloDebugging/Program.cs
+++ HalloDebugging/HalloDebugging/Program.cs
@@ -10,6 +10,30 @@ namespace HalloDebugging
     {
         static void Main(string[] args)
         {
+            // F5:      Starten mit Debugger
+            // STRG+F5: Starten ohne Debugger
+
+            // F10:     Einzelschritt ohne Hineinspringen in Methoden
+            // F11:     Einzelschritt mit Hineinspringen in Methoden
+            Console.WriteLine("Hallo Welt");
+
+            int erg = Addieren(12, 5);
+
+            Console.WriteLine(erg);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
+
+        public static int Addieren(int z1,int z2)
+        {
+            return z1 + z2;
+        }
+
     }
 }
