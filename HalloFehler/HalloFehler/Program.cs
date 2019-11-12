7a7204ea3569b896ab781b299d39b7f69315a5b7
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-12 09:33:26 +0100

    Neuer Fehler mit mehr Daten im Stack

--- HalloFehler/HalloFehler/Program.cs
+++ HalloFehler/HalloFehler/Program.cs
@@ -12,9 +12,15 @@ namespace HalloFehler
         {
             Console.WriteLine("Bitte geben Sie eine Zahl ein:");
             string eingabe = Console.ReadLine();
+            bool flag = false;
+            string demo = "demostring 12345";
+            StringBuilder sb = new StringBuilder();
+            sb.Append(demo);
+
             int zahl = Convert.ToInt32(eingabe);
             Console.WriteLine($"Das doppelte der eingegebenen Zahl ist {zahl * 2}");
 
+            throw new DivideByZeroException();
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
