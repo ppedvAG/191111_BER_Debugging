7d9b711bc1414535ef03e37c3ea5a9e4212d8df8
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-12 09:53:56 +0100

    Thread-Fenster im Debugging-Modus angeschaut

--- HalloFehlerImTask/HalloFehlerImTask/Program.cs
+++ HalloFehlerImTask/HalloFehlerImTask/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HalloFehlerImTask
@@ -10,6 +11,44 @@ namespace HalloFehlerImTask
     {
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(MachWasInEinemThread);
+            Thread t2 = new Thread(MachWasInEinemThread2);
+            Thread t3 = new Thread(MachWasInEinemThread3);
+
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        private static void MachWasInEinemThread()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write("#");
+            }
+        }
+        private static void MachWasInEinemThread2()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                Thread.Sleep(80);
+                Console.Write("_");
+            }
+        }
+
+        private static void MachWasInEinemThread3()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write("X");
+            }
         }
     }
 }
