87fe28505f7948f98c1760b77cee15e9c00db041
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-12 09:16:22 +0100

    Programm mit einem Fehler

--- HalloFehler/HalloFehler/Program.cs
+++ HalloFehler/HalloFehler/Program.cs
@@ -10,6 +10,13 @@ namespace HalloFehler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+            int zahl = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Das doppelte der eingegebenen Zahl ist {zahl * 2}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
