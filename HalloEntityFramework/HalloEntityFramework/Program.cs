70b57e8b3d795f93cb21aa89236d52133131f8ec
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-12 14:03:20 +0100

    Entity Framework

--- HalloEntityFramework/HalloEntityFramework/Program.cs
+++ HalloEntityFramework/HalloEntityFramework/Program.cs
@@ -10,6 +10,33 @@ namespace HalloEntityFramework
     {
         static void Main(string[] args)
         {
+            // Entity Framework: DatabaseFirst
+            // Anleitung:
+            // 1) NewItem > Ado.NET EntityFramwork Model
+            // 2) Entity Framework Code First DatabaseFirst auswählen
+            // 3) Connection auswählen und die Tabellen der DB auswählen
+
+            Model1 meinModel = new Model1();
+
+            // Daten auslesen
+            var alleCustomers = meinModel.Customers.ToList();
+            foreach (var item in alleCustomers)
+            {
+                Console.WriteLine($"{item.CompanyName}");
+            }
+
+            Customers neuerCustomer = new Customers();
+            neuerCustomer.ContactName = "Michael Zöhling";
+            neuerCustomer.CompanyName = "ppedv AG";
+            neuerCustomer.CustomerID = "MZ200";
+            // Insert:
+            meinModel.Customers.Add(neuerCustomer);
+
+            // Speichern (automatisch in einer Transaktion)
+            meinModel.SaveChanges();
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
